f373e5042b4274f7503081fdb7b9f502a1c47526
Interim President of the USA <shauntmcgovern@gmail.com>
2022-06-04 14:09:09 -0500

    the Ostrich class

--- birds.cs
+++ birds.cs
@@ -54,3 +54,16 @@ class Pigeon : Bird
 		return eggs;
 	}
 }
+
+class Ostrich : Bird
+{
+	public override Egg[] LayEggs(int numberOfEggs)
+	{
+		Egg[] eggs = new Egg[numberOfEggs];
+		for (int i = 0; i < numberOfEggs; i++)
+		{
+			eggs[i] = new Egg(Bird.Randomizer.NextDouble() + 12, "speckled");
+		}
+		return eggs;
+	}
+}
