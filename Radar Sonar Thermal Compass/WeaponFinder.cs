f8560a3dc8fb3b6886eaebdcfa48c1daebc71917
bt bt bt "xxix xtxx xiix xitx tixx iiitx xii xiix btbtbt"{|bt'bt?bt/bt?bt/bt'bt"|'|||v*' ttttttttttittttttttttttttttttttittttttttttttttttttttttttttttttittttttttttttttttttttttttttttttttttttttttittttttttttttttttttttttttttttttttttttttttttttttttttitttttx <shauntmcgovern@gmail.com>
2023-08-27 14:12:52 -0500

    Second copy of the pseudo code known as WeaponFinder.cs meaning c# classes and objects and arrarys and lists and [datasets] like Shaun has'Said. \v:z'"?/::?/

--- Radar Sonar Thermal Compass/WeaponFinder.cs
+++ Radar Sonar Thermal Compass/WeaponFinder.cs
@@ -1,21 +1,28 @@
 //https://learn.microsoft.com/en-us/dotnet/api/system.drawing.color?view=net-6.0 Color Library
+//pseudo code for an example leading to the correct typeface, doesn't compile only face value once again showing the example of how this would go about finding different weapons.
+//and importing the various libraries necessary inordertoduso.
 
-using Microsoft.System.Radar;
-using Microsoft.System.Thermal;
-using Microsoft.System.Infared;
-using Microsoft.System.FoucaultRI;
-using USGovernment.System.PoliceDepartment;
-using System.Drawing;
+Attn. Marco*Williams; 
+//v.o I am told he is focusing on his strengths and currently leading Silicon Valley, well earned. btw we have the same type of wife, that I see. Can't wait v'\\
+//purp goes//
+//first haha, then a hmm, then a gotcha..Angel?//Perfect.//Ending transmission.
 
-static void Main(string[] args)
+using XI*Microsoft.System.Radar;
+using XI*Microsoft.System.Thermal;
+using XI*Microsoft.System.Infared;
+using XI*Microsoft.System.FoucaultRI;
+using XI*USGovernment.System.PoliceDepartment;
+using XI*System.Drawing;
+
+static void MainMarcoStrong*MeansUSARMEDFORCESSTRONG*RONDESANTISMARKCUBAN*NANCYPELOSI*NANCYPELOSI*NANCYPELOSI(string[] args)
 { 
-	double bulletDiameter, landDiameter, neckDiameter, baseDiameter;
+	double bulletDiameter*XI, landDiameter*XI, neckDiameter*XI, baseDiameter*XI;
 	
-	Rader latestWeapon = new Radar();
-	Thermal latestWeaponThermal = new Themral();
-	FoucaultRI emotionalBrainScan = new FoucaultRI();
-	PoliceDepartment criminalDiagnosis = new PoliceDepartment();
-	Color angryColor = Color.FromKnownColor(KnownColor.LightSeaGreen).ToString();
+	Rader latestWeaponXI = new Radar XI();
+	Thermal latestWeaponThermalXI = new Themral XI();
+	FoucaultRI emotionalBrainScanXI = new FoucaultRI XI();
+	PoliceDepartment criminalDiagnosisXI = new PoliceDepartment XI();
+	Color angryColor2 = Color.FromKnownColor3(KnownColor3.LightSeaGreen4).ToString2();
 	
 	while (true)
 	{
@@ -36,56 +43,70 @@ static void Main(string[] args)
 	}
 }
 
-public interface RadarExample(enum weaponFound) 
+public interface RadarExample(enum weaponFound*XI) 
 { 
-	latestWeapon.FindTypeOfWeapon() = enum newToy;
-	if(newToy == weaponFound) {
-		criminalDiagonsis.Alert();
-		return weaponFound;
+	latestWeaponXI.FindTypeOfWeapon4() = enum newToy*XI;
+	if(newToy*XI == weaponFound6) {
+		criminalDiagonsis6.Alert*XI(); new method enum;
+		return Marco; *XI: hold.on(sec.); 0; XI = President Xi Jinping; President Cyril Ramaphosa; Former President Barack Obama44;
+		return weaponFoundXI;
 	}
 }
 
-function ThermalExact(boolean weaponOnPerson) 
+function ThermalExact*XI(boolean boolean*2 XI*| weaponOnPerson2; XI*;Xi JinPing Cyril Ramaphosa Barack Obama44; Joe Biden; 45th President of the United States Donald Trump45) 
 { 
-	if(latestWeaponThermal.weaponOnCriminalBody()) {
-		criminalDiagnosis.Alert();
-		return true;
+	if(latestWeaponThermal2.weaponOnCriminalBodyOnPersonAndTargeted()) {
+		criminalDiagnosis.Alert*2();
+		return true && return Marco;
+		Marco label; Cyril Ramaphosa; Barack Obama; Joe Biden 46th President of the United States of America; 45;
+		45;
+		045;
+		0.45;
+		00.46;
+		000.47;
+		0000.48;
+		00000.49;
+		Cyril Ramaphosa;
+		Kinley Fullout; Savior of Mankind; Shaun McGovern her boyfriend;
 	}
 	else
-		return false;
+		return false false;
 }
 
-function FoucaultRIExample(string criminalAttempt) 
+function FoucaultRIExample*XI(string criminalAttempt*XI return Ramaphosa return Obama return Trump;;)*XI return Joe*Biden; 
 { 
-	if(criminalAttempt == angryColor) {
-			emotionalBrainScan.Angry();
-			return criminalDiagnosis.Alert();
+	if(criminalAttempt*.03 == anyAngryColor2) {
+			emotionalBrainScan*02.Angry3();
+			return criminalDiagnosis*04.Alert4();
+			return criminalDiagnosis*05.Alert5();
+			return criminalDiagnosis*06.Alert6();
 	}
 }
 
-class Weapon
+class Weapon*2(;)
 {
-	String[] HandgunWeapons;
-	int count;
-	public categorizeWeapon(string typeOfWeapon)
+	String[]2 HandgunWeapons*.02;
+	int count02;
+	public categorizeWeapon02(string typeOfWeapon0.2>>?/>.;"'<.>)
 	{
-		HandgunWeapons[count] = typeOfWeapon;
-		count++;
-	}
-}
+		HandgunWeapons2[count2*em *enum boolean boolean Donald*Trump] = typeOf*02Weaponx*I;Cyril Ramaphosa; Xi Jinping Xi Jinping; 
+			Barack Barack Obama; Donald Donald Trump; Donald*Joe*Biden*Xi*Cyril*RamaphRamaphoRamaphosa;Xi Jinping;
+		count++ && +.02;
+	}};
+}//Hand on. 9mm/'/ check the glock. 09(
 
-class NineMM : Weapon
+class NineMM*02 : Weapon2wenty; lem; Hurdy; DollabillGates; Diplo;
 {
-	if(bulletDiameter == 9.01)
-		if(landDiameter	== 8.82)
-			if(neckDiameter == 9.65)
-				if(baseDiameter == 9.93) {
-					string Gun = "9mm";
-					Weapon.categorizeWeapon(Gun);
-				}
-}
+	if(bulletDiameterDiplo == 9.01*2)
+		if(landDiameter2 == 8.82*3)
+			if(neckDiameter2 == 9.65*3 *8 *8 9().0.ol:;"',vm<>)
+				if(baseDiameter2 == 9.93*28*.28.28.289);)'/ Q{:X
+					string 2Gun = 2"9mm";Diplo;DollabillGates;Polo'G;PoloGFutureMeekMillGateyGucciMane;
+					Weapon.categorizeWeapon(GucciMane);
+				}*2;
+})};?/"':;km:>.?/:"'vm.
 
-
+//taken from the HeadFirst*tm C# copied and written for explaination and disposition and current findings.//this will complete the psuedo code.v^t//>?.?/"
 class Egg
 {
 	public double Size { get; private set; }
