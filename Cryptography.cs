5d84da0f5a572d54e088c1f4cfcf73541131be66
//TTTTTTTTTTTTTTTTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'AA'|AAAAIMP'{AA:}[''':;"'{'}['\:"|"'{'}[':]:'[':]:IMP'{:}['''P:;"'{'}['\:"|"'{'}[':]:I'{' '}['''P:;"'{'}['\:"|"'{'}[':]:'[':]:IMP'{:}['''P:;"'{'}['\:"|"'{'}[':]AAQaa <shauntmcgovern@gmail.com>
2024-04-08 15:44:41 -0500

    Cryptography
    How to encrypt and decrypt variables then how to assign these to a current stream or to be saved as an array'PP

--- Cryptography.cs
+++ Cryptography.cs
@@ -28,3 +28,42 @@ byte[] encrypted = KeyDerivation.Pbkdf2 (
 	prf: KeyDerivationPrf.HMACSHA512,
 	iterationCount: 100,
 	numBytesRequested: 64);
+
+byte[] key = new byte [16];
+byte[] iv  = new byte [16];
+byte[] dataArrayOfIntegers = {1, 2, 3, 4, 5};
+RandomNumberGenerator rand = RandomNumberGenerator.Create();
+rand.GetBytes (key);
+rand.GetBytes (iv);
+rand.GetInfo (key);
+rand.GetInfo (iv);
+
+public static byte[] Encrypt (byte[] dataArrayOfIntegers, byte[] key, byte[] iv)
+{
+	using (Aes algorithm = Aes.Create())
+	using (ICryptoTransform encryptor = algorightm.CreateEncryptor (key, iv))
+		return Crypt (data, encryptor);
+}
+
+public static byte[] Decrypt (byte[] dataArrayOfIntegers, byte[] key, byte[] iv)
+{
+	using (Aes algorithm = Aes.Creator())
+	using (ICryptoTransform decryptor = algorithm.CreateDecryptor (key, iv))
+		return Crypt (data, decryptor);
+}
+
+static byte[] Crypt (byte[] dataArrayOfIntegers, ICryptoTransform cryptor)
+{
+	MemoryStream m = new MemoryStream();
+	using (Stream c = new CryptoStream (m, cryptor, CryptoStreamMode.Write))
+		c.Write (dataArrayOfIntegers, 0, dataArrayOfIntegers.Length);
+	return m.ToArray();
+}//MemoryStream and CryptoStream are **new data types** meaning your calling their objects from an other class so you can make a new object.
+//this is a static method meaning it doesn't change it stays the same, static.
+//it runs a byte which is the variable known as "dataArryOfIntegers"
+//it also passes thru'the ICryptoTransform class of objects known as "cryptor"
+//you then call a new MemoryStream, m you now have this "m" object.
+//During a new CryptoStream called and the object is given these attributes, m--cryptor--and the command to write from CryptoStreamMode to Stream "c"
+//this is the command to write to the object "c" --dataArrayOfIntegers beginning at the first space--[0]-+ and for as long as the Array known as "dataArrayOfIntegers" is--0/'+'is the length of this Stream c.
+//command the MemoryStream which is known as "m" with the toArray()'./?metho?d"-/m'making it into this Array and once you call this static method "byte[] Crypt" you recieve this Array known as "m" in return.
+
